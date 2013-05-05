8081d065d5496d248506a7f36d49c404552dec29
Juan RP <xtraeme@gmail.com>
2013-05-05 08:18:51 +0200

    Update with a working cross setup slave for rpi.

--- scripts/user_settings.py
+++ scripts/user_settings.py
@@ -5,6 +5,7 @@ machines = [
 		'http_git_url': 'https://github.com/xtraeme/xbps-packages',
 		'workdir': 'i686-workdir', # created on the master
 		'hostdir': '/hostdir',  # must exist on the slave
+		'crosstarget': 'native',
 		'slave_name': 'i686_void',
 		'slave_pass': 'SLAVE_PASSWORD',
 		'admin': 'user1'
@@ -15,9 +16,21 @@ machines = [
 		'http_git_url': 'https://github.com/xtraeme/xbps-packages',
 		'workdir': 'x86_64-workdir', # created on the master
 		'hostdir': '/hostdir', # must exist on slave
+		'crosstarget': 'native',
 		'slave_name': 'x86_64_void',
 		'slave_pass': 'SLAVE_PASSWORD',
 		'admin': 'user2'
+	}
+	{
+		'name': 'user3',
+		'mach': 'armv6l',
+		'http_git_url': 'https://github.com/xtraeme/xbps-packages',
+		'workdir': 'cross-rpi-workdir', # created on the master
+		'hostdir': '/hostdir', # must exist on slave
+		'crosstarget': 'armv6hf',
+		'slave_name': 'crossrpi_void',
+		'slave_pass': 'SLAVE_PASSWORD',
+		'admin': 'user3'
 	}
 ]
 
