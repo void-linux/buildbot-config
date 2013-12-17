47c670d2e964b65e57913504779e2f935df6c7c9
Juan RP <xtraeme@gmail.com>
2013-12-17 17:48:27 +0100

    Sync with current buildbot config.

--- scripts/user_settings.py
+++ scripts/user_settings.py
@@ -2,7 +2,6 @@ machines = [
 	{
 		'name': 'user1',
 		'mach': 'i686',
-		'http_git_url': 'https://github.com/xtraeme/xbps-packages',
 		'workdir': 'i686-workdir', # created on the master
 		'hostdir': '/hostdir',  # must exist on the slave
 		'crosstarget': 'native',
@@ -13,7 +12,6 @@ machines = [
 	{
 		'name': 'user2',
 		'mach': 'x86_64',
-		'http_git_url': 'https://github.com/xtraeme/xbps-packages',
 		'workdir': 'x86_64-workdir', # created on the master
 		'hostdir': '/hostdir', # must exist on slave
 		'crosstarget': 'native',
@@ -24,7 +22,6 @@ machines = [
 	{
 		'name': 'user3',
 		'mach': 'armv6l',
-		'http_git_url': 'https://github.com/xtraeme/xbps-packages',
 		'workdir': 'cross-rpi-workdir', # created on the master
 		'hostdir': '/hostdir', # must exist on slave
 		'crosstarget': 'armv6hf',
